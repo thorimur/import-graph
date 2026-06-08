fc46714121c63040e3a18b514098ec7068f32483
thorimur <thomasmurrills@gmail.com>
2026-06-08 18:42:16 -0400

    feat(Precedes): depths

--- ImportGraph/Shake/Precedes.lean
+++ ImportGraph/Shake/Precedes.lean
@@ -31,6 +31,7 @@ instance : Union Postcedes where
 def Postcedes.hierarchyLe (p₁ p₂ : Postcedes) : Bool := p₁.previous.le p₂.previous
 
 -- unbundled?
+-- TODO: Okay, I need a way to chain all these things
 
 partial def Lean.Environment.mkTransDeps (env : Environment) : Array Needs := Id.run do
   let mut transDeps := Array.mkEmpty env.header.moduleData.size
@@ -69,3 +70,20 @@ partial def Lean.Environment.mkPrevious (env : Environment) : Array Bitset := Id
       prev := prev ∪ {j} ∪ prevs[j]!
     prevs := prevs.push prev
   return prevs
+
+partial def Lean.Environment.mkPreviousWithDepths (env : Environment)
+    (skipInit := true) : Array Bitset × Array Nat := Id.run do
+  let mut prevs := Array.mkEmpty env.header.moduleData.size
+  let mut depths := Array.mkEmpty env.header.moduleData.size
+  for i in 0...env.header.moduleData.size do
+    let mod := env.header.moduleData[i]!
+    let mut prev := {}
+    let mut depth := 0
+    for imp in mod.imports do
+      unless !skipInit || (`Init).isPrefixOf imp.module do
+        let j := env.getModuleIdx! imp.module
+        prev := prev ∪ {j} ∪ prevs[j]!
+        depth := max depth (depths[j]! + 1)
+    prevs := prevs.push prev
+    depths := depths.push depth
+  return (prevs, depths)
