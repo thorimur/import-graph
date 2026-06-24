dea713e08525f9e745361b98d6bba6ba5c414cdb
thorimur <68410468+thorimur@users.noreply.github.com>
2026-06-24 17:33:13 -0400

    feat(Coverings): for find home

--- ImportGraph/Shake/Coverings.lean
+++ ImportGraph/Shake/Coverings.lean
@@ -2,6 +2,7 @@ module
 
 import all ImportGraph.Shake.Environment
 import all ImportGraph.Shake.Algebra
+import Std.Data.HashMap.AdditionalOperations
 
 open ImportGraph Lean Lake Shake
 
@@ -16,19 +17,60 @@ We also want to consider different notions of "available". For instance: "where 
 -- /-- Finds the modules `i` that provide `needs` according to `transDeps` (including `i`'s public and
 -- private scopes), and are lowest according to `lt`. `needs` does not need to be
 -- transitively closed, nor does `transDeps` need to be filled. -/
+
+def ImportGraph.Preceding.compareByDepthThenSize (p₁ p₂ : Preceding) :=
+  compare p₁.depth p₂.depth |>.then <| compare p₁.prev.size p₂.prev.size
+
 def Needs.coveringsByProject (s : ImportGraph.Shake.StateWithPreceding) (needs : Needs) :
-    Array (Name × (Array (ModuleIdx × Preceding))) := Id.run do
+    Std.HashMap Name (Array (ModuleIdx × Preceding)) := Id.run do
   let mut minimals : Std.HashMap Name (Array (Option (ModuleIdx × Preceding))) := {}
+  let some allKey := s.toKeyStore.getIdxOf? .anonymous | return {}
   for i in 0...s.transDeps.size do
     if needs.coveredBy i s.transDeps then
       -- TODO: be cleverer about this? Can we skip entire attempts?
       -- Or maybe totally different data structure? List, perhaps?
       -- Traversing in one direction or another
-      for val in (id s.vals[i]! : Array _), key in s.ofIdx do
-        if let some val@{ participating := true, .. } := val then
-          minimals := minimals.incorporateBelowAt key (i, val) fun (_,p₁) (_,p₂) =>
-            p₁.prev.lt p₂.prev
-  return minimals.toArray.map fun (key, vals) => (key, vals.reduceOption)
+      if let some allVal := s.vals[i]![allKey]?.join then
+        for val in s.vals[i]!, key in s.ofIdx do
+          if let some { participating := true, .. } := val then
+            minimals := minimals.incorporateBelowAt key (i, allVal) fun (_,p₁) (_,p₂) =>
+              p₁.prev.lt p₂.prev
+  return minimals.map (fun _ arr => arr.reduceOption.qsort fun (_,p₁) (_,p₂) =>
+    p₁.compareByDepthThenSize p₂ |>.isLT)
+
+structure FindHomeResult where
+  inCurrentByCurrent : Array (ModuleIdx × Preceding) := #[]
+  inAnyByAll : Std.HashMap Name (Array (ModuleIdx × Preceding)) := {}
+
+def FindHomeResult.isEmpty : FindHomeResult → Bool
+  | { inCurrentByCurrent, inAnyByAll } =>
+    inCurrentByCurrent.isEmpty && inAnyByAll.all fun _ arr => arr.isEmpty
+
+/-- Single-purpose function for the current implementation of `#find_home`. -/
+def Needs.coveringsFindHome (s : ImportGraph.Shake.StateWithPreceding) (needs : Needs)
+    (currentRoot : Name) : FindHomeResult := Id.run do
+  let mut inCurrentByCurrent : Array (Option <| ModuleIdx × Preceding) := #[]
+  let mut inAnyByAll : Std.HashMap Name (Array (Option <| ModuleIdx × Preceding)) := {}
+  let some allKey := s.toKeyStore.getIdxOf? .anonymous | return {}
+  for i in 0...s.transDeps.size do
+    if needs.coveredBy i s.transDeps then
+      -- TODO: be cleverer about this? Can we skip entire attempts?
+      -- Or maybe totally different data structure? List, perhaps?
+      -- Traversing in one direction or another
+      let allVal? := s.vals[i]![allKey]?.join
+      for val? in s.vals[i]!, key in s.ofIdx do
+        if let some val@{ participating := true, .. } := val? then
+          if key == currentRoot then
+            -- `participating := true` above means it's in the current project.
+            -- Adjust both `inCurrent*`s within this branch.
+            inCurrentByCurrent := inCurrentByCurrent.incorporateBelow (i, val) fun (_,p₁) (_,p₂) =>
+              p₁.prev.lt p₂.prev
+          if let some allVal := allVal? then
+            inAnyByAll := inAnyByAll.incorporateBelowAt key (i, allVal) fun (_,p₁) (_,p₂) =>
+              p₁.prev.lt p₂.prev
+  return {
+    inCurrentByCurrent := inCurrentByCurrent.reduceOption
+    inAnyByAll := inAnyByAll.map fun _ arr => arr.reduceOption }
 
 -- -- Are there different senses of `lt`? Should this be a `KeyedArray Bool` or such?
 -- def PrecedingVals.lt (ps₁ ps₂ : KeyedArray Preceding) : Bool :=
