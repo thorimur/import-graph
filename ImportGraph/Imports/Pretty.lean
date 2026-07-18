a644cdc0571d8b8ecf57d9d199889154c63630f7
thorimur <68410468+thorimur@users.noreply.github.com>
2026-07-17 23:03:47 -0400

    chore(Format): move andList

--- ImportGraph/Imports/Pretty.lean
+++ ImportGraph/Imports/Pretty.lean
@@ -6,6 +6,7 @@ Authors: Thomas R. Murrills
 module
 
 public import Lean.Setup
+public import ImportGraph.Lean.Format
 
 meta import Lean.Parser.Module.Syntax
 import ImportGraph.Lean.Syntax
@@ -314,18 +315,6 @@ def collectWithWhitespaceFromSource (newImps : Array Import)
       reviewWhitespace := reviewWhitespace.push (module, actualImps, differentlyImported)
 
   return (impsWithWs, { unusedWithComments, multipleTrailing, reviewWhitespace : FormatErrors })
-
-/--
-Puts `Format` into a comma-separated list with `"and"` at the back (with the serial comma).
-
-Best used on non-empty lists; returns `"– none –"` for an empty list.
--/
-def _root_.Std.Format.andList [ToFormat α] (xs : List α) : Format :=
-  match xs with
-  | [] => "– none –"
-  | [x] => format x
-  | [x₀, x₁] => f!"{x₀} and {x₁}"
-  | xs@(_ :: _ :: _) => f!"{f!", ".joinSep xs.dropLast}, and {xs.getLast (by grind)}"
 
 instance : ToFormat FormatErrors where
   format | { multipleTrailing, reviewWhitespace, unusedWithComments } => Id.run do
