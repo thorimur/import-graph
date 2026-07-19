292d73715344581ef5e547e54cd3a6cb8c941391
thorimur <68410468+thorimur@users.noreply.github.com>
2026-07-19 19:32:20 -0400

    chore(RunLater): docs

--- ImportGraph/Util/RunLater.lean
+++ ImportGraph/Util/RunLater.lean
@@ -18,6 +18,7 @@ initialize runReporter : Backreporter (Array Syntax → CommandElabM Unit) ←
         let savedState ← get
         try
           request.data cmds
+          -- Wait for the message to be reported instead of running `request.markCompleted` here.
         catch
           | Exception.error ref msg =>
             logException (.error ref m!"Backreporting request failed: {msg}")
@@ -26,12 +27,20 @@ initialize runReporter : Backreporter (Array Syntax → CommandElabM Unit) ←
         finally
           modify fun s => { savedState with messages := s.messages, traceState := s.traceState }
 
-/-- Runs `x` at the end of the file. -/
+/-- Runs `x` at the end of the file.
+
+If `showProgress := true` (the default) and `Elab.async` is `true`, creates a yellow bar which
+disappears once `x` is run. Uses `ref?` for the location of the bar if provided; otherwise uses the
+ambient `ref`. -/
 def runLater (x : CommandElabM Unit) (ref? : Option Syntax := none) (showProgress := true) :
     CommandElabM Unit :=
   runReporter.sendRequest (fun _ => x) ref? showProgress
 
-/-- Runs `f` at the end of the file on the module's full `Array Syntax`. -/
+/-- Runs `f` at the end of the file on the module's full `Array Syntax`.
+
+If `showProgress := true` (the default) and `Elab.async` is `true`, creates a yellow bar which
+disappears once `x` is run. Uses `ref?` for the location of the bar if provided; otherwise uses the
+ambient `ref`. -/
 def runLaterWithSyntax (f : Array Syntax → CommandElabM Unit)
     (ref? : Option Syntax := none) (showProgress := true) :
     CommandElabM Unit :=
