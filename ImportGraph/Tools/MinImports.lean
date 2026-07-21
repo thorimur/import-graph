21f451a47b76daa95ec74cfc98c3441b62d9252e
thorimur <68410468+thorimur@users.noreply.github.com>
2026-07-21 02:13:38 -0400

    feat(MinImports): first draft

--- ImportGraph/Tools/MinImports.lean
+++ ImportGraph/Tools/MinImports.lean
@@ -12,8 +12,10 @@ public meta import ImportGraph.Lean.Environment
 public meta import ImportGraph.Imports.FromSource
 public meta import ImportGraph.Lean.Syntax
 public meta import ImportGraph.Shake.Algebra
+public meta import ImportGraph.Shake.DeclNeeds
 public meta import ImportGraph.Shake.Environment
 public meta import ImportGraph.Tools.NeedsGrid
+public import ImportGraph.Util.RunLater
 
 import Lean
 import ImportGraph.Lean.Environment
@@ -23,25 +25,90 @@ import Lake.CLI.Shake
 
 import all ImportGraph.Shake.Environment
 import all ImportGraph.Tools.NeedsGrid
+import all ImportGraph.Shake.DeclNeeds
+import all Lean.Elab.Command -- for `recordUsedSyntaxKinds`
+import all ImportGraph.Shake.EnvExtension
 
-public meta section
+open ImportGraph Lean Elab Command Lake Shake
 
-open Lean
+#check calcDeclNeeds
 
-/--
-Return the names of the modules in which constants used in the current file were defined,
-with modules already transitively imported removed.
+meta def getModuleNeeds (cmds : Array Syntax) : CommandElabM Needs := do
+  for cmd in cmds do
+    recordUsedSyntaxKinds cmd
+  let mut declNeeds : DeclNeeds := ∅
+  for (decl, _) in (← getEnv).constants.map₂ do
+    declNeeds := calcDeclNeeds decl (← getEnv) declNeeds
+  -- logInfo m!"DeclNeeds: {declNeeds.toArray.map fun (decl, declneed) =>
+  --   have fixed : Array (ModuleIdx × NameMap (NeedsKindSet × Bool)) := declneed.fixedDecls.toArray
+  --   let fixed := fixed.flatMap fun a : ModuleIdx × NameMap (NeedsKindSet × Bool) => a.2.toArray.map fun a : Name × (NeedsKindSet × Bool) => m!"{MessageData.ofConstName a.1}{a.2.1.toArray}"
+  --   m!"{MessageData.ofConstName decl}: {fixed}"}"
+  for (decl, need) in declNeeds do
+    let env ← getEnv
+    logInfo m!"{decl} requires {need.fixedDecls.keysArray.map (env.header.modules[·]!.module)}\n\
+      Std.Do.Triple.SpecLemmas: {need.fixedDecls[env.getModuleIdx! `Std.Do.Triple.SpecLemmas]?.map (·.toArray.map fun (a, b) => (MessageData.ofConstName a, b.2))}"
+  let mut needs := declNeeds.fullNeeds
+  let (modUses, _) := getExtraModUsesState (← getEnv)
+  logInfo m!"Extra mod uses: {modUses.map (·.module)}"
+  for { module, isExported, isMeta } in modUses do
+    let idx := (← getEnv).getModuleIdx! module
+    needs := needs.union { isExported, isMeta } {idx}
+  -- Note: current indirect mod uses are not relevant, since they are only signals to downstream
+  -- modules to import the current module.
+  return needs
 
-Note that this will *not* account for tactics and syntax used in the file,
-so the results may not suffice as imports.
--/
-@[deprecated
-  "Use `Environment.mkTransDeps` and `Needs.reduce` to handle imports in the module system"
-  (since := "2026-07-18")]
-def Lean.Environment.minimalRequiredModules (env : Environment) : Array Name :=
-  let required := env.requiredModules.toArray.erase env.header.mainModule
-  let redundant := findRedundantImports env required
-  required.filter fun n => ¬ redundant.contains n
+-- TODO: process shake annotations instead of leaving them in the error.
+elab tk:"#min_imports" : command => do
+  runLaterWithSyntax (ref? := tk) fun cmds => do
+    let needs ← getModuleNeeds cmds
+    let needs := needs.reduce (← getEnv).mkTransDeps
+    let reducedImps := needs.toRawImports (← getEnv)
+    -- TODO: transitive
+    let reducedImps := Import.includeAll (← getEnv).header.imports reducedImps
+
+    let (header, _, log) ← parseCurrentHeader
+    if log.hasErrors then
+      -- This should be impossible.
+      throwError m!"The current imports failed to parse. Errors:\n\
+        {m!"\n".joinSep <| log.toList.map (·.data)}"
+    let sourceImps := headerToImportRefsWithWhitespace header
+
+    let some (msg, errs) ← liftCoreM <| Import.mkImportSuggestionMessage tk reducedImps sourceImps
+      | logInfo m!"Imports are minimal."
+    let formattingChangeAtMost := Import.beqUpToOrder (sourceImps.map (·.1.toImport)) reducedImps
+    -- TODO: if imports are the same but not normalized, different message
+    if errs.isEmpty then
+      if formattingChangeAtMost then
+        logInfo m!"Imports can be reformatted, but are otherwise minimal:{msg}"
+      else
+        logWarning m!"Imports can be reduced:{msg}"
+    else
+      if formattingChangeAtMost then
+        logInfo m!"Imports can be reformatted, but are otherwise minimal.\n\n\
+          However, some comments could not be carried over. \
+          Please review the source comment that will be inserted after the imports.\n\
+          {msg}"
+      else
+        logWarning m!"Imports can be reduced, but some comments could not be carried over. \
+          Please review the source comment that will be inserted after the imports.\n\
+          {msg}"
+
+-- #min_imports
+
+-- /--
+-- Return the names of the modules in which constants used in the current file were defined,
+-- with modules already transitively imported removed.
+
+-- Note that this will *not* account for tactics and syntax used in the file,
+-- so the results may not suffice as imports.
+-- -/
+-- @[deprecated
+--   "Use `Environment.mkTransDeps` and `Needs.reduce` to handle imports in the module system"
+--   (since := "2026-07-18")]
+-- def Lean.Environment.minimalRequiredModules (env : Environment) : Array Name :=
+--   let required := env.requiredModules.toArray.erase env.header.mainModule
+--   let redundant := findRedundantImports env required
+--   required.filter fun n => ¬ redundant.contains n
 
 -- #norm_imports
 -- #min_imports
@@ -64,6 +131,7 @@ def Lean.Environment.minimalRequiredModules (env : Environment) : Array Name :=
 --     eventuallyEncountered := {eventuallyEncountered.size}\n\
 --     notEncountered := {notEncountered.size}\n\
 --     {outOfOrder.toArray.qsort (·.1.2 < ·.1.2)}"
+
 
 
 -- /--
